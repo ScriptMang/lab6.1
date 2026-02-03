41e58d905876ce72b903fe1044373621c103b1e3
scriptMang <aperalta686@gmail.com>
2026-02-03 17:59:45 -0500

    Rewrite sort-products func to use builtin array sort method

--- src/models/FilterProducts.ts
+++ src/models/FilterProducts.ts
@@ -10,20 +10,6 @@ class FilterProduct {
     }
 
     sortProducts(productList: Array<Product>): Array<Product> {
-        for (let i= 0; i < productList.length; i++) {
-            let elemA: Product = productList[i] || new Product("123456", "placeholderA", 4); 
-            for (let j=i+1;  j < i; j--) {
-                if (this.filterByPrice) {
-                    let elemB: Product = productList[j] || new Product("450385", "placeholderB", 34);
-                    if (elemB.price > elemA.price) {
-                        let tempElem = elemB;
-                        productList[j] = elemA;
-                        productList[i] = tempElem;
-                    }
-                }
-            }
-        }
-       return productList;
+       return productList.sort((a: Product, b: Product) => a.price - b.price);
     }
-    
 }
